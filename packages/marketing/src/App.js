7c252dd59c8e7a42fd4ea344b0891edba8129963
jfarrow02 <jack.dempsey.farrow@gmail.com>
2023-10-01 12:20:50 -0400

    completes ch09

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma', // generates all class names with a prefix of `ma` for PRODUCTION, in our `marketing` app ONLY
+});
 
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
